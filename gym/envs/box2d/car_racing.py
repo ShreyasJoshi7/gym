aa413dc54157b08f7dee048d84bfd926241edf16
Shreyas Joshi <30567439+ShreyasJoshi7@users.noreply.github.com>
2017-11-18 12:57:37 +0530

    Update car_racing.py

--- gym/envs/box2d/car_racing.py
+++ gym/envs/box2d/car_racing.py
@@ -2,7 +2,7 @@ import sys, math
 import numpy as np
 
 import Box2D
-from Box2D.b2 import (edgeShape, circleShape, fixtureDef, polygonShape, revoluteJointDef, contactListener)
+from Box2D import (edgeShape, circleShape, fixtureDef, polygonShape, revoluteJointDef, contactListener)
 
 import gym
 from gym import spaces
